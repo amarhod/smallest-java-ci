f8b984c388d1ef3326f3a20033e0302f2c91e967
Amar Hodzic <amar_hodzic@hotmail.com>
2021-02-12 13:36:47 +0100

    feat: Added function that verifies that the Github webhook POST is from a specific branch, tests included #7

--- app/src/test/java/smallest/java/ci/GitHelperTest.java
+++ app/src/test/java/smallest/java/ci/GitHelperTest.java
@@ -1,17 +1,21 @@
 package smallest.java.ci;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.TransportException;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import static smallest.java.ci.FsHelper.removeFolder;
 import static smallest.java.ci.GitHelper.cloneRepo;
+import static smallest.java.ci.GitHelper.isValidWebhook;
 
 public class GitHelperTest {
+    
     @Test
     void cloneRepoTest() throws IOException, GitAPIException {
         File newDirectory = new File("testFolder");
@@ -37,5 +41,27 @@ public class GitHelperTest {
             cloneRepo("testFolder2", "nonExistingBranch");
         });
         removeFolder("testFolder2");
+    }
+
+    @Test
+    void isValidWebhookTest() throws IOException, ParseException {
+        BufferedReader payload;
+        String currentPath = System.getProperty("user.dir") + "/src/test/java/smallest/java/ci/";
+        String filepath = currentPath + "payload.json";
+        try {
+            payload = new BufferedReader(new FileReader(filepath));
+        }catch(IOException e){throw new IOException("payload test file missing");}
+        Assertions.assertEquals(true, isValidWebhook(payload, "webhook-signals"));
+
+        try {
+            payload = new BufferedReader(new FileReader(filepath));
+        }catch(IOException e){throw new IOException("payload test file missing");}
+        Assertions.assertEquals(false, isValidWebhook(payload, "nonExistingBranch"));
+
+        filepath = currentPath + "badPayload.json";
+        try {
+            payload = new BufferedReader(new FileReader(filepath));
+        }catch(IOException e){throw new IOException("payload test file missing");}
+        Assertions.assertEquals(false, isValidWebhook(payload, "nonExistingBranch"));
     }
 }
