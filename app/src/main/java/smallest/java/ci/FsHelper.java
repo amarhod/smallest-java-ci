d5847a7bbf21799d4a474d0f5f3aa7cb3fefc045
Amar Hodzic <amar_hodzic@hotmail.com>
2021-02-15 23:40:45 +0100

    feat: Added saveBuildInfo function that summerizes and saves commit and build info onto a local file #16

--- app/src/main/java/smallest/java/ci/FsHelper.java
+++ app/src/main/java/smallest/java/ci/FsHelper.java
@@ -2,15 +2,37 @@ package smallest.java.ci;
 
 
 import org.eclipse.jgit.util.FileUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FsHelper {
+
     static void removeFolder(String filepath) throws IOException {
         File dir = new File(filepath);
         try {
             FileUtils.delete(dir, FileUtils.RECURSIVE);
             } catch (IOException e) {}
+    }
+
+    static void saveBuildInfo(String gradleBuildInfo, String payload, String commitHash) throws IOException {
+        JSONParser parser = new JSONParser();
+        JSONObject payloadObject;
+        try {
+            payloadObject = (JSONObject) parser.parse(payload);
+        }catch(ParseException e){return;}
+        JSONObject headCommit = (JSONObject) payloadObject.get("head_commit");
+        String commitMsg = (String) headCommit.get("message");
+        JSONObject pusher = (JSONObject) headCommit.get("author");
+        String author = (String) pusher.get("name");
+
+        String commitSummary = "\nAuthor: " + author + "\nCommit: " + commitHash + "\nCommit message: " + commitMsg + '\n';
+        String finalSummary =  commitSummary + gradleBuildInfo;
+        File buildMeta = new File("buildMetaInfo/" + commitHash);
+        try (PrintWriter out = new PrintWriter("buildMetaInfo/" + commitHash)){
+            out.println(finalSummary + '\n');
+        }
     }
 }
